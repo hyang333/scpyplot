8d143b2bf035598e9a2127b30ffa9457b228902e
Huan Yang <iamyanghuan@gmail.com>
2023-01-19 03:40:51 -0600

    Update scpyplot.py

--- scpyplot.py
+++ scpyplot.py
@@ -3,7 +3,7 @@
 # ylablel_pos: (left, bottom), axes fraction coords
 # fontsize: 13
 
-def arrowAxes(ax=ax, xlabel_pos=(0.26, 0.005), ylabel_pos=(0.005, 0.28), fontsize=13):
+def arrowAxes(ax, xlabel_pos=(0.26, 0.005), ylabel_pos=(0.005, 0.28), fontsize=13):
     ax.axis('off')
     ax.annotate("", xy=(0.02, 0.015), xycoords='axes fraction', textcoords='axes fraction', xytext=(0.02, 0.25), arrowprops=dict(arrowstyle="<-"))
     ax.annotate("", xy=(0.015, 0.02), xycoords='axes fraction', textcoords='axes fraction', xytext=(0.25,0.02), arrowprops=dict(arrowstyle="<-"))
