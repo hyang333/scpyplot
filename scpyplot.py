15f883e912eea7fb34fd7b5c9941142f27861cfd
Huan Yang <iamyanghuan@gmail.com>
2023-01-19 03:18:45 -0600

    Update scpyplot.py

--- scpyplot.py
+++ scpyplot.py
@@ -1,7 +1,9 @@
 
-def arrowAxes(ax=ax)
+def arrowAxes(ax=ax, xlabel=(0.26, 0.005), ylabel=(0.005, 0.28), fontsize=13):
     ax.axis('off')
     ax.annotate("", xy=(0.02, 0.015), xycoords='axes fraction', textcoords='axes fraction', xytext=(0.02, 0.25), arrowprops=dict(arrowstyle="<-"))
     ax.annotate("", xy=(0.015, 0.02), xycoords='axes fraction', textcoords='axes fraction', xytext=(0.25,0.02), arrowprops=dict(arrowstyle="<-"))
-    ax.text(.26, 0.01, s='UMAP-1', transform=ax.transAxes)
-    ax.text(0.01, 0.26,  s='UMAP-2', transform=ax.transAxes, rotation=90)
+    ax.text(xlabel[0], xlabel[1], s='UMAP-1', transform=ax.transAxes, fontsize=fontsize)
+    ax.text(ylabel[0], ylabel[1],  s='UMAP-2', transform=ax.transAxes, rotation=90, fontsize=fontsize)
+    
+    return ax
