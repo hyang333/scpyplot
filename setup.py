d2ddb53fc695f0c33f23e154441c8fada9f6f439
Huan Yang <iamyanghuan@gmail.com>
2023-01-19 03:37:58 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+__version__ = "0.0.1"
+
+
+setup(
+    name="scpyplot",
+    version=__version__,
+    author="Huan Yang",
+    author_email="iamyanghuan@gmail.com",
+    url="",
+    description="",
+    install_requires=[
+       
+    ],
+
+)
